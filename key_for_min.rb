623624da2214ac35ac1a63962fee1174108a3a8a
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-10 20:21:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smalval = 10000
+  smalkey = ""
+  name_hash.do |key, value|
+    
 end
